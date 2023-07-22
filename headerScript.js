5ce53c55869d11ee4bf13c9a490dc2fbb4ed5278
David Knittel <djknit@gmail.com>
2023-07-22 02:47:48 -0500

    initial commit. pushing everything, including junk

---
+++ headerScript.js
@@ -0,0 +1,60 @@
+const HEADER_ID = 'pg-sticky-header';
+const SPACER_ID = 'pg-sticky-head-spacer';
+
+$(document).ready(function() { 
+    runMeasuringProcess(50, 2000);
+});
+
+$(window).on('resize', function() {
+    resetStyles();
+    runMeasuringProcess();
+});
+
+
+function runMeasuringProcess(tries, waitTime) {
+    const isTriesSpecified = typeof tries == 'number';
+    if (isTriesSpecified && tries <= 0) {
+        return;
+    }
+    const headerHeight = getHeaderHeight();
+    if (headerHeight) {
+        setSpacerHeight(headerHeight);
+    }
+    else if (isTriesSpecified) {
+        setTimeout(
+            function() {
+                runMeasuringProcess(--tries)
+            },
+            typeof waitTime == 'number' ? waitTime : 150
+        );
+    }
+}
+
+function getJqueryFor(elId) {
+    return $('#' + elId);
+}
+
+function getHeaderHeight() {
+    const headerEl = getJqueryFor(HEADER_ID);
+    if (headerEl && headerEl.outerHeight) {
+        return headerEl.outerHeight(false);
+    }
+}
+
+function setSpacerHeight(height) {
+    if (!height)return;
+    const spacerEl = getJqueryFor(SPACER_ID);
+    const headerEl = getJqueryFor(HEADER_ID);
+    headerEl.css('position', 'fixed');
+    spacerEl.prop(
+        'style',
+        'display:block!important; position:static!important; width:auto:!important; height:' + height + 'px!important; max-height:' + height + 'px!important;'
+    );
+}
+
+function resetStyles() {
+    const spacerEl = getJqueryFor(SPACER_ID);
+    const headerEl = getJqueryFor(HEADER_ID);
+    headerEl.prop('style', 'width:100%;');
+    spacerEl.prop('style', 'display:none;');
+}
